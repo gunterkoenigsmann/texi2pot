b9ac91b75f1ec7b7989041c6bbc941d8b5ef69f8
Gunter Königsmann <gunter@peterpall.de>
2017-05-19 19:46:22 +0200

    A copyright note as the GPL requests it.

--- texi2pot.pl
+++ texi2pot.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/perl
 
+    # texi2pot: Split a .texi file into strings accessible to msgmerge
+    # Copyright (C) {2017}  {Gunter Königsmann}
+
+    # This program is free software: you can redistribute it and/or modify
+    # it under the terms of the GNU General Public License as published by
+    # the Free Software Foundation, either version 3 of the License, or
+    # (at your option) any later version.
+
+    # This program is distributed in the hope that it will be useful,
+    # but WITHOUT ANY WARRANTY; without even the implied warranty of
+    # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    # GNU General Public License for more details.
+
+    # You should have received a copy of the GNU General Public License
+    # along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 print "# This file contains all translatable strings from a .texi file.\n";
 print "# Copyright (C) YEAR THE PACKAGE'S COPYRIGHT HOLDER\n";
 print "# This file is distributed under the same license as the PACKAGE package.\n";
